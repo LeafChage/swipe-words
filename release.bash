73c169201d40b2d603b250c916720594d04cec46
LeafChage <t.kusanagi5512@gmail.com>
2026-07-21 15:28:46 +0900

    ok

--- release.bash
+++ release.bash
@@ -5,6 +5,7 @@ if ! [ -n "$(git tag --list | grep $version)" ]; then
     git tag -a "$version" -m "$version"
     git push origin "$version"
     echo "success"
+    echo "you can relase, on https://github.com/LeafChage/obsidian-anki-card/releases"
 else
     echo "already released, you should version up refering to manifest.json" 1>&2
 fi
